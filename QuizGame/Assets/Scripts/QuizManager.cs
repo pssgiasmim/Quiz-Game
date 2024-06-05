9bc4b681eb50a7d3725bfd2dd8749591045c70a9
pssgiasmim <iasmimpassing@gmail.com>
2024-06-05 11:39:06 -0300

    Continuação do projeto de Quiz
    Eu (Iasmim) baixei o arquivo do professor e dei continuidade na aula.

--- QuizGame/Assets/Scripts/QuizManager.cs
+++ QuizGame/Assets/Scripts/QuizManager.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class QuizManager : MonoBehaviour
 {
-    private Quiz[] quizList;
-    private Quiz currentQuiz;
+     [SerializeField] private Quiz[] quizList;
+     [SerializeField] private Quiz currentQuiz;
+
+     [SerializeField] Quiz.Theme theme;
+     [SerializeField] Quiz.Dificulty dificulty;
+
 
     public void SelectQuiz(Quiz.Theme themeSelected, Quiz.Dificulty dificultySelected)
     {
-        Quiz quiz = quizList[Random.Range(0, quizList.Length)];
+        Quiz quiz = quizList[Random.Range(0, quizList.Length)]; 
         if(quiz.GetDificulty == dificultySelected && quiz.GetTheme == themeSelected)
         {
             currentQuiz = quiz;
+            UIManager.instance.UpdateQuestion(currentQuiz);
         }
         else
         {
             SelectQuiz(themeSelected, dificultySelected);
         }
     }
+
+    
 }
